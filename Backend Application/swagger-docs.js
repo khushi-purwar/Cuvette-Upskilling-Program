8c2c8df52a696c604cb2d83a478a09c78f27b2e4
Khushi Purwar <khushipurwar4@gmail.com>
2021-09-19 15:34:21 +0530

    add swagger docs

--- Backend Application/swagger-docs.js
+++ Backend Application/swagger-docs.js
@@ -20,21 +20,155 @@ const options = {
       },
     ],
 
+    tags: [
+      {
+        name: "User operations",
+      },
+      {
+        name: "Product operations",
+      },
+      {
+        name: "Cart operations",
+      },
+    ],
+
     components: {
-        schemas: {
-          // user model
-          id: {
-            type: "string", // data type
-            description: "An id of a todo", // desc
-            example: "tyVgf", // example of an id
+      schemas: {
+        // user model
+        User: {
+          type: "object",
+          properties: {
+            firstName: {
+              type: "string",
+              description: "First name of the user",
+              example: "Khushi",
+            },
+            lastName: {
+              type: "string",
+              description: "Last name of the user",
+              example: "Purwar",
+            },
+            username: {
+              type: "string",
+              description: "username will be unique",
+              example: "khushiP123",
+            },
+            email: {
+              type: "string",
+              description: "Email of the user",
+              example: "khushi123@gmail.com",
+            },
+            passwrod: {
+              type: "string",
+              description: "Password",
+              example: "khushipur123",
+            },
+            role: {
+              type: "string",
+              enum: ["Consumer", "Admin", "Supplier"],
+              description: "Role of the user",
+              example: " Admin ",
+            },
+          },
+        },
+        // product schema
+        Product: {
+          type: "object",
+          properties: {
+            name: {
+              type: "string",
+              description: "Name of the product",
+              example: "Watch",
+            },
+            price: {
+              type: "number",
+              description: "price of the product",
+              example: "12999",
+            },
+            qty: {
+              type: "number",
+              description: "Quantity of the product",
+              example: "100",
+            },
+            desc: {
+              type: "string",
+              description: "About the product",
+              example:
+                "LCS-8188 BLUE DIAL AND SILVER STRAP DAY & DATE FUNCTIONING WATCH FOR BOYS Analog Watch - For Men",
+            },
+            productPictures: {
+              type: "array",
+              description: "Product pictures",
+            },
           },
         },
       },
+    },
+
+    paths: {
+      "/api/v1/consumer/signup": {
+        post: {
+          tags: ["User operations"],
+          description: "Register Consumer",
+          operationId: "registerconsumer",
+          parameters: [
+            {
+                "-in": "body",
+               name: "body",
+               description: "",
+               required: true,
+               schema: {
+                email : "khushi123@gmail.com",
+                password : "khushi123"
+              },
+            }
+             ],
+          responses: {
+            200: {
+              description: "Consumer register successfully",
+              content: {
+                // content-type
+                "application/json": {
+                  schema: {
+                    $ref: "#/components/schemas/User", 
+                  },
+                },
+              },
+            },
+            400: {
+              description: "Something went wrong!",
+             
+            },
+          },
+        },
+      },
+      "/api/v1/consumer/signin": {
+        post: {
+          tags: ["User operations"],
+          description: "Login Consumer",
+          operationId: "loginconsumer",
+          parameters: [
+         {
+              "-in": "body",
+            name: "body",
+            description: "",
+            required: true,
+            schema:
+           { $ref: "#/components/schemas/user"}
+         }
+          ],
+          responses: {
+            200: {
+              description: "Consumer login successfully",
+
+            },
+          },
+        },
+      },
+    },
   },
   apis: ["./routes/api/v1/*.js"],
- 
 };
-
 
 const spec = swaggerJsDoc(options);
 
